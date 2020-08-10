4350dfefca471fe337b399caed4a8a16b8b310e2
dkgiant <dangkynguyhiem@gmail.com>
2020-08-10 14:52:45 +0700

    - create function & signup page

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
-# Create your views here.
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signup.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signup.html',
+                              {'form': UserCreationForm(), 'error': 'That Username has already taken'})
+
+        else:
+            return render(request, 'todo/signup.html', {'form': UserCreationForm(), 'error': 'password not match'})
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
