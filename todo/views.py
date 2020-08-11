1e0a8da4022ef4af21f0195d77d4563f5775cd6d
dkgiant <dangkynguyhiem@gmail.com>
2020-08-11 13:56:02 +0700

    add function todo list: - create - current list

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from .forms import TodoForm
+from .models import Todo
 
 
 def home(request):
@@ -45,5 +47,21 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'value error'})
+
+
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
