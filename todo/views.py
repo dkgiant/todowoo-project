1e29742b015053ffc32156bae85aeb7cf6b2c715
dkgiant <dangkynguyhiem@gmail.com>
2020-08-11 10:24:09 +0700

    - complete basic auth function

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
@@ -32,6 +32,18 @@ def logoutuser(request):
         logout(request)
         return redirect('home')
 
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/login.html',
+                          {'form': AuthenticationForm(), 'error': 'Username and password dit not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -23,10 +23,10 @@ urlpatterns = [
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
     # Todos
     path('', views.home, name='home'),
-
     path('current/', views.currenttodos, name='currenttodos'),
 
 ]
